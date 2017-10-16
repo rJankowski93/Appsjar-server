220363ec3f6562d4b7250ad36b85af22e1d705d7
Karol Klimończyk <k.klimonczyk@gmail.com>
2017-10-16 19:13:28 +0200

    Init commit.

---
+++ src/main/kotlin/com/krkcoders/apps/jar/AppsJarApplication.kt
@@ -0,0 +1,11 @@
+ package com.krkcoders.apps.jar
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AppsJarApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(AppsJarApplication::class.java, *args)
+}
