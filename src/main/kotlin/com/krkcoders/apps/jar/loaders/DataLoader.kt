d1605a8c68cbf997d38959f9a0a5b3e9b5640a43
Karol Klimończyk <k.klimonczyk@gmail.com>
2018-01-23 12:43:30 +0100

    -added sample data and styles

--- src/main/kotlin/com/krkcoders/apps/jar/loaders/DataLoader.kt
+++ src/main/kotlin/com/krkcoders/apps/jar/loaders/DataLoader.kt
@@ -24,6 +24,7 @@ class DataLoader @Autowired constructor(private val appRepository: AppRepository
         appRepository.save(listOf(
                 App(ObjectId.get().toString(), "Facebook", "1.0.0", "v=cNfK6LNuGa4", SampleDate.descriptions.description1, 3.0, SampleDate.icons.facebookIcon),
                 App(ObjectId.get().toString(), "Twitter", "1.1.3", "v=cNfK6LNuGa4", SampleDate.descriptions.description1, 3.0, SampleDate.icons.twitterIcon),
+                App(ObjectId.get().toString(), "TicTacToe", "2.5.3", "v=cNfK6LNuGa4", SampleDate.descriptions.description1, 3.0, SampleDate.icons.twitterIcon),
                 App(ObjectId.get().toString(), "Asphalt", "2.1.3", "v=cNfK6LNuGa4", SampleDate.descriptions.description1, 4.0, SampleDate.icons.asphaltIcon),
                 App("1", "EverNote", "3.1.3", "v=cNfK6LNuGa4", SampleDate.descriptions.description1, 5.0, SampleDate.icons.evernoteIcon),
                 App("2", "BallGame", "4.1.3", "v=cNfK6LNuGa4", SampleDate.descriptions.description1, 2.0, SampleDate.icons.ballGameIcon)
@@ -33,7 +34,7 @@ class DataLoader @Autowired constructor(private val appRepository: AppRepository
     private fun saveSampleUsersToDb() {
         userRepository.deleteAll()
         userRepository.save(listOf(
-                User(ObjectId.get().toString(), "admin", "admin", listOf(appRepository.findByName("Facebook"), appRepository.findByName("Asphalt"), appRepository.findByName("App4"))),
+                User(ObjectId.get().toString(), "admin", "admin", listOf(appRepository.findByName("Facebook"), appRepository.findByName("Asphalt"), appRepository.findByName("BallGame"))),
                 User(ObjectId.get().toString(), "user2", "user2", appRepository.findAll()),
                 User(ObjectId.get().toString(), "user3", "user3", emptyList())
         ))
